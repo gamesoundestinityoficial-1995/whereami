ee14ab53b6f7755d17445cc835eff7f6ff697bad
gamesoundestinityoficial-1995 <gamesoundestinity.oficial@gmail.com>
2026-03-21 01:18:26 +0100

    test table regions

--- api/users.js
+++ api/users.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     }
 
     const sql = neon(process.env.DATABASE_URL);
-    const rows = await sql`SELECT NOW() as hora`;
+    const rows = await sql`SELECT * FROM regions`;
 
     return res.status(200).json(rows);
   } catch (error) {
